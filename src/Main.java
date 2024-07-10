165aa2abe9f2239818cbacd15404f7b85907d59f
zaidragib1 <zaidragib1@gmail.com>
2024-07-10 06:39:33 +0530

    Adding Test Case where PlayerB will surely win

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Test Case 1: Anyone can win!
         Player playerA = new Player("Player A", 50, 5, 10);
         Player playerB = new Player("Player B", 100, 10, 5);
-
+        System.out.println("Anyone can win!");
         Game game = new Game(playerA, playerB);
+        game.start();
+
+        // Test Case 2: Making sure Player B will win!
+        playerA = new Player("Player A", 50, 5, 10);
+        playerB = new Player("Player B", 100, 10, 5);
+        playerA.reduceHealth(50);
+        game = new Game(playerA, playerB);
+        System.out.println("Making sure Player B will win!");
         game.start();
     }
 }
